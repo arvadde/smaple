3ff22e3d08b5ef4f6d114339a5f679e8a4ac0f75
Venu <Venu@192.168.2.7>
2019-05-26 11:34:32 +0530

    first commit

---
+++ SpringMVC/src/main/resources/Batch.java
@@ -0,0 +1,20 @@
+import javax.batch.runtime.JobExecution;
+
+import org.springframework.batch.core.JobParametersBuilder;
+
+import com.sun.org.apache.bcel.internal.ExceptionConstants;
+
+public class Batch
+{
+	//JobExecution je=null;
+	JobExecution je=null;
+public static void main(String[] args)
+{
+	Batch b=new Batch();
+	b.jobs();
+}
+public void jobs(){
+//	
+System.out.println("hai");
+}
+}
